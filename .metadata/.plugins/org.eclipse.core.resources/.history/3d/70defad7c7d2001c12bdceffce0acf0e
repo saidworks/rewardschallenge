951447eca4a02e654e3a716ffe6175990ea89e82
Said ZITOUNI <zitouni.sd@gmail.com>
2022-05-13 10:23:04 -0400

    create model and repository start working on controller add new reward method

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/3d/70defad7c7d2001c12bdceffce0acf0e
@@ -0,0 +1,7 @@
+package com.fetchrewards.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RewardRepository extends JpaRepository<Reward, Long> {
+
+}
---
+++ CodingExercice/src/test/java/com/fetchrewards/rewards/CodingExerciceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetchrewards.rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingExerciceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
