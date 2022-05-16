6ec39e27ac2967a4d2a6cf1e49e4e0038c47ed18
Said ZITOUNI <zitouni.sd@gmail.com>
2022-05-16 16:44:03 -0400

    delete old project folder

--- CodingExercice/src/main/java/com/fetchrewards/rewards/repository/RewardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fetchrewards.rewards.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fetchrewards.rewards.model.Reward;
-
-public interface RewardRepository extends JpaRepository<Reward, Long> {
-
-}
